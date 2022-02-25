76fb9c4255cd40fc12517932dacf6b6e12a0fc04
maofulong <maofulong@pccpa.cn>
2022-02-25 15:10:31 +0800

    Initial commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'antd';
+
+class Home extends React.Component {
+    render() {
+        return (<Button>I am a Button</Button>)
+    }
+}
+
+export default Home;
